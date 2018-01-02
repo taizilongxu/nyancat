68994ee9cb5fc13737d3745c88b88b15ee2f4142
James Socol <James_Socol@condenast.com>
2018-01-02 13:47:01 -0500

    Lint python code
    Make nyancat.py follow pep8 rules so it's easier to read and for others
    to contributes

--- nyancat/nyancat.py
+++ nyancat/nyancat.py
@@ -4,24 +4,26 @@ from frame import frames
 import subprocess
 import time
 
+
 class Nyancat(object):
     FRAME_HEIGHT = FRAME_WIDTH = 64  # frame size
     COLORS = {
-            ',' :  "\033[48;5;17m",
-            '.' :  "\033[48;5;231m",
-            '\'':  "\033[48;5;16m",
-            '@' :  "\033[48;5;230m",
-            '$' :  "\033[48;5;175m",
-            '-' :  "\033[48;5;162m",
-            '>' :  "\033[48;5;196m",
-            '&' :  "\033[48;5;214m",
-            '+' :  "\033[48;5;226m",
-            '#' :  "\033[48;5;118m",
-            '=' :  "\033[48;5;33m",
-            ';' :  "\033[48;5;19m",
-            '*' :  "\033[48;5;240m",
-            '%' :  "\033[48;5;175m"
+        ',': '\033[48;5;17m',
+        '.': '\033[48;5;231m',
+        "'": '\033[48;5;16m',
+        '@': '\033[48;5;230m',
+        '$': '\033[48;5;175m',
+        '-': '\033[48;5;162m',
+        '>': '\033[48;5;196m',
+        '&': '\033[48;5;214m',
+        '+': '\033[48;5;226m',
+        '#': '\033[48;5;118m',
+        '=': '\033[48;5;33m',
+        ';': '\033[48;5;19m',
+        '*': '\033[48;5;240m',
+        '%': '\033[48;5;175m',
     }
+
     def __init__(self):
         self.terminal_height, self.terminal_width = self.linesnum()
         self.min_col = (self.FRAME_WIDTH - self.terminal_width/2) / 2
@@ -46,37 +48,43 @@ class Nyancat(object):
             print '\033[?25l]'
             for y in range(self.min_row, self.max_row):
                 for x in range(self.min_col, self.max_col):
-                    if  23 < y < 43 and x < 0:
-                        mod_x = ((-x+2) % 16) / 8;
-                        if (i / 2) % 2: mod_x = 1 - mod_x;
+                    if 23 < y < 43 and x < 0:
+                        mod_x = ((-x+2) % 16) / 8
+                        if (i / 2) % 2:
+                            mod_x = 1 - mod_x
                         tmp = mod_x + y - 23
                         if -1 < tmp < len(rainbow):
                             color = rainbow[tmp]
                         else:
                             color = ','
-                    elif x < 0 or y < 0 or y >= self.FRAME_HEIGHT or x >= self.FRAME_WIDTH:
+                    elif (x < 0 or y < 0 or
+                            y >= self.FRAME_HEIGHT or x >= self.FRAME_WIDTH):
                         color = ','
                     else:
                         color = frames[i][y][x]
                     if self.always_escape:
                         print self.COLORS[color],
                     else:
-                        if color != last and self.COLORS.has_key(color):
-                            last = color;
+                        if color != last and color in self.COLORS:
+                            last = color
                             print self.COLORS[color]+self.output,
                         else:
                             print self.output,
                 print
             i += 1
-            if i == 11: i = 0
+            if i == 11:
+                i = 0
             last = 0
             time.sleep(0.1)
+
     def __del__(self):
         print '\033[?25h]'
+
 
 def main():
     nyancat = Nyancat()
     nyancat.run()
+
 
 if __name__ == '__main__':
     main()
